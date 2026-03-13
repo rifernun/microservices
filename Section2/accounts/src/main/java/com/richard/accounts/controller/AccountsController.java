8b8749006b589abeb8a8797863d73067761bb7dc
Rifernun <rifernun@gmail.com>
2026-03-13 20:09:46 -0300

    feat: Implement createAccount endpoint in AccountsController

--- Section2/accounts/src/main/java/com/richard/accounts/controller/AccountsController.java
+++ Section2/accounts/src/main/java/com/richard/accounts/controller/AccountsController.java
@@ -1,13 +1,21 @@
 package com.richard.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.richard.accounts.constants.AccountsConstants;
+import com.richard.accounts.dto.CustomerDto;
+import com.richard.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE}) //manter esse padrao
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello() {
-        return "Hi, World!";
+    @PostMapping(path = "/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
