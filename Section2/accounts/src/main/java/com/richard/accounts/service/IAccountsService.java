94acdcbe57d5e0e09f6569eb90064de0e8bf294b
Rifernun <rifernun@gmail.com>
2026-03-14 10:33:16 -0300

    refactor: update method signatures in IAccountsService

--- Section2/accounts/src/main/java/com/richard/accounts/service/IAccountsService.java
+++ Section2/accounts/src/main/java/com/richard/accounts/service/IAccountsService.java
@@ -2,10 +2,7 @@ package com.richard.accounts.service;
 
 import com.richard.accounts.dto.CustomerDto;
 
-/**
- *
- * @param customerDto - CustomerDto Object
- */
+
 public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
 }
