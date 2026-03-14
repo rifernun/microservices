545d7ff92c6a0f24bfb5b3526e5dbf1cd1ebbc6f
Rifernun <rifernun@gmail.com>
2026-03-14 10:34:02 -0300

    feat: implement create method Account and API Method

--- Section2/accounts/src/main/java/com/richard/accounts/controller/AccountsController.java
+++ Section2/accounts/src/main/java/com/richard/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.richard.accounts.controller;
 import com.richard.accounts.constants.AccountsConstants;
 import com.richard.accounts.dto.CustomerDto;
 import com.richard.accounts.dto.ResponseDto;
+import com.richard.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE}) //manter esse padrao
+@AllArgsConstructor //gera um construtor com todos os argumentos, ou seja, com todas as dependencias da classe, ou seja, o IAccountsService
 public class AccountsController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping(path = "/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
--- Section2/accounts/src/main/java/com/richard/accounts/service/impl/AccountsServiceImpl.java
+++ Section2/accounts/src/main/java/com/richard/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,19 @@
 package com.richard.accounts.service.impl;
 
+import com.richard.accounts.constants.AccountsConstants;
 import com.richard.accounts.dto.CustomerDto;
+import com.richard.accounts.entity.Accounts;
+import com.richard.accounts.entity.Customer;
+import com.richard.accounts.exception.CustomerAlreadyExistsException;
+import com.richard.accounts.mapper.CustomersMapper;
 import com.richard.accounts.repository.AccountRepository;
 import com.richard.accounts.repository.CustomerRepository;
 import com.richard.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +24,28 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomersMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer Already Exists with Mobile Number: " + customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountRepository.save(createNewAccount(savedCustomer));
 
+    }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts accounts = new Accounts();
+        accounts.setCustomerId(customer.getCustomerId());
+        long randomAccountNumber = (long) (Math.random() * 10000000000L);
+
+        accounts.setAccountNumber(randomAccountNumber);
+        accounts.setAccountType(AccountsConstants.SAVINGS);
+        accounts.setBranchAddress(AccountsConstants.ADDRESS);
+        accounts.setCreatedAt(LocalDateTime.now());
+        accounts.setCreatedBy("Anonymous");
+        return accounts;
     }
 }
