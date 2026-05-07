4083ed87d25e6c5dbf2ace82d8d5aa924290b874
Rifernun <rifernun@gmail.com>
2026-05-07 12:10:11 -0300

    feat: Add global exception handler for generic exceptions

--- Section2/accounts/src/main/java/com/richard/accounts/exception/GlobalExceptionHandler.java
+++ Section2/accounts/src/main/java/com/richard/accounts/exception/GlobalExceptionHandler.java
@@ -12,6 +12,17 @@ import java.time.LocalDateTime;
 @ControllerAdvice //se acontecer algum erro em QUALQUER lugar da aplicação, ele vai cair aqui e tratar o erro
 public class GlobalExceptionHandler extends RuntimeException {
 
+    @ExceptionHandler(Exception.class) //se acontecer um erro do tipo CustomerAlreadyExistsException, ele vai cair aqui e tratar o erro
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception ex, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false), //retorna o caminho da requisição que causou o erro
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CustomerAlreadyExistsException.class) //se acontecer um erro do tipo CustomerAlreadyExistsException, ele vai cair aqui e tratar o erro
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest webRequest) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
@@ -33,4 +44,5 @@ public class GlobalExceptionHandler extends RuntimeException {
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
     }
+
 }
