5d6c6e3aab6e25e7ebb4279f422870e6b8fda228
Rifernun <rifernun@gmail.com>
2026-05-07 12:12:12 -0300

    feat: Add validation annotations to AccountsDto fields

--- Section2/accounts/src/main/java/com/richard/accounts/dto/AccountsDto.java
+++ Section2/accounts/src/main/java/com/richard/accounts/dto/AccountsDto.java
@@ -1,10 +1,19 @@
 package com.richard.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+
+    @NotEmpty(message = "Account number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type cannot be empty")
     private String accountType;
+
+    @NotEmpty(message = "Branch Address cannot be empty")
     private String branchAddress;
 }
