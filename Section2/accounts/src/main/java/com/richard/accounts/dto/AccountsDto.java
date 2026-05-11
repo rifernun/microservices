8d7dc7bc7a37df114d61d678434457c4a776665a
Rifernun <rifernun@gmail.com>
2026-05-11 10:18:52 -0300

    feat: Update validation for account number in AccountsDto to ensure it is not null

--- Section2/accounts/src/main/java/com/richard/accounts/dto/AccountsDto.java
+++ Section2/accounts/src/main/java/com/richard/accounts/dto/AccountsDto.java
@@ -1,14 +1,13 @@
 package com.richard.accounts.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
-    @NotEmpty(message = "Account number cannot be empty")
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @NotNull(message = "Account number cannot be null")
     private Long accountNumber;
 
     @NotEmpty(message = "Account type cannot be empty")
